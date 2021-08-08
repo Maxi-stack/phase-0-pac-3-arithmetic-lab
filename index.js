10cb7bf7b66a2e62ffab14e70b46fc0bf9dd5e54
Maxi-stack <85815496+Maxi-stack@users.noreply.github.com>
2021-08-08 18:48:39 +1000

    done

---
+++ index.js
@@ -0,0 +1,59 @@
+function add(a, b) {
+    var a = 1;
+    var b = 2;
+    return (a + b);
+}
+
+function subtract(a, b) {
+    var a = 1;
+    var b = 2;
+    return (a - b);
+}
+
+function multiply(a, b) {
+    var a = 3;
+    var b = 1;
+    return (a * b);
+}
+
+function divide(a,b) {
+    var a = 3;
+    var b = 1;
+    return (a / b);
+}
+
+//------------
+
+function add(a, b) {
+    return (a + b);
+}
+
+function subtract(a, b) {
+    return (a - b);
+}
+
+function multiply(a, b) {
+    return (a * b);
+}
+
+function divide(a,b) {
+    return (a / b);
+}
+
+function increment(n) {
+return (n+=1);
+}
+
+function decrement(n) {
+return (n-=1);
+    }
+
+//------------
+
+function makeInt(n) {
+	return (parseInt(n,10));
+}
+
+function preserveDecimal(n) {
+	return (parseFloat(n));
+}
